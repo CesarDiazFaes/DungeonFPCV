b0f1d61d95c4314e82a2777c3d7562501e37c05e
DESKTOP-UE07BU8\Cesar <cesardiazfaes@ucm.es>
2017-07-15 22:26:28 +0200

    First commit, all the project

---
+++ PuebaCV3D/Assets/Scripts/PlayerActions.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class PlayerActions : MonoBehaviour {
+
+    public GameObject lanternObj;
+
+    private Light lantern_light;
+    private AudioSource aud;
+    private bool isLanternActived = false;
+	
+	void Start () {
+        this.lantern_light = this.lanternObj.GetComponent<Light>();
+        this.lantern_light.enabled = this.isLanternActived;
+        this.aud = this.lanternObj.GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q))
+        {
+            this.turnLantern();
+        }
+	}
+
+    private void turnLantern()
+    {
+        this.isLanternActived = !this.isLanternActived;
+
+        this.aud.Play();
+        this.lantern_light.enabled = this.isLanternActived;
+    }
+}
